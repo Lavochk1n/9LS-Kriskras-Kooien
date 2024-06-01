5b8cbf0fbd4772caaf5b0d18c6f3a9a56dcb9c8a
csbruijn <corsnee@gmail.com>
2024-06-01 10:08:55 +0200

    ScoreSubmitter

---
+++ Gamelab 9LS- URP DA Game/Assets/Score & leaderboard/Leaderboard/Local Leaderboard/Scripts/PlayerHighScoreSubmitter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHighScoreSubmitter : MonoBehaviour
+{
+    private string name1, name2, teamName;
+    private int score; 
+
+
+
+    public void SetName1(string name)
+    {
+        name1 = name;
+    }
+
+    public void SetName2(string name)
+    {
+        name2 = name;
+    }
+    public void SetTeamName(string name)
+    {
+        teamName = name;
+    }
+
+
+    public void Submit()
+    {
+        if (name1 == null || name2 == null || teamName == null)  return;  
+
+        Debug.Log(teamName + ": " +  name1 + " & " + name2 );
+
+        ScenesManager.Instance.GetMainMenu();
+    }
+}
