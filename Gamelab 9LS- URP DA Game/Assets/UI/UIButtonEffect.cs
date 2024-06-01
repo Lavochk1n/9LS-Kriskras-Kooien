db16ff232f7624a49c571c2c455182ca38b4bb5d
miabuckley <miaavabuckley@gmail.com>
2024-06-01 13:58:44 +0200

    Main Menu UI working!!
    Added a UI Manager, UI manager script, UI effect script too!.
    
    main menu now functioning!!

---
+++ Gamelab 9LS- URP DA Game/Assets/UI/UIButtonEffect.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class UIButtonEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    public float fontSizeIncrease = 1f; // Amount to increase font size by
+    private TextMeshProUGUI text; // The TextMeshPro component
+    private GameObject graphic; // The background graphic
+    private float originalFontSize; // To store the original font size
+    private FontStyles originalFontStyle; // To store the original font style
+
+    void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        graphic = transform.Find("BackgroundGraphic")?.gameObject;
+
+        if (text != null)
+        {
+            originalFontSize = text.fontSize;
+            originalFontStyle = text.fontStyle;
+        }
+
+        if (graphic != null)
+        {
+            graphic.SetActive(false); // Hide graphic initially
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ShowHoverEffect();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HideHoverEffect();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        ShowHoverEffect();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        HideHoverEffect();
+    }
+
+    private void ShowHoverEffect()
+    {
+        if (graphic != null)
+        {
+            graphic.SetActive(true); // Show graphic
+        }
+
+        if (text != null)
+        {
+            text.fontSize = originalFontSize + fontSizeIncrease; // Increase font size
+            text.fontStyle = FontStyles.Bold; // Set text to bold
+        }
+    }
+
+    private void HideHoverEffect()
+    {
+        if (graphic != null)
+        {
+            graphic.SetActive(false); // Hide graphic
+        }
+
+        if (text != null)
+        {
+            text.fontSize = originalFontSize; // Reset to original font size
+            text.fontStyle = originalFontStyle; // Reset to original font style
+        }
+    }
+}
+
