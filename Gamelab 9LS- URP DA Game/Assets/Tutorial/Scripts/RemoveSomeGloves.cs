a864382d754b276a658927d0a3c5c95bb7118dcf
csbruijn <corsnee@gmail.com>
2024-06-05 21:22:06 +0200

    no ui room  0, gloves start at 2 room 1

---
+++ Gamelab 9LS- URP DA Game/Assets/Tutorial/Scripts/RemoveSomeGloves.cs
@@ -0,0 +1,39 @@
+using Quarantine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveSomeGloves : MonoBehaviour
+{
+    private bool foundPlayers;
+
+    private GloveManager p1, p2;
+
+    [SerializeField] private int glovesToRemove = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!foundPlayers)
+        {
+            p1 = QuarentineManager.Instance.player.GetComponent<GloveManager>();
+            p2 = QuarentineManager.Instance.player2.GetComponent<GloveManager>();
+
+            if (p1 != null && p2 != null)
+            {
+                foundPlayers = true;
+
+                for (int i = 0; i < glovesToRemove; i++)
+                {
+                    p1.RemoveGlove();
+                    p2.RemoveGlove();
+                }
+                
+            }
+            else return;
+        }
+
+        
+
+    }
+}
