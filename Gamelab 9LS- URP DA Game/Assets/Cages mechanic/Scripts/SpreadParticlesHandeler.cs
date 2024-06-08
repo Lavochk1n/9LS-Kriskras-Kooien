358021edc8233e90ff09684dbefcacf1f4a10622
csbruijn <corsnee@gmail.com>
2024-06-08 21:53:32 +0200

    slight increase in edge distance

--- Gamelab 9LS- URP DA Game/Assets/Cages mechanic/Scripts/SpreadParticlesHandeler.cs
+++ Gamelab 9LS- URP DA Game/Assets/Cages mechanic/Scripts/SpreadParticlesHandeler.cs
@@ -17,7 +17,7 @@ public class SpreadParticlesHandeler : MonoBehaviour
 
     public List<PSRef> psRefList = new();
 
-    private float edgeDistance = .35f;
+    private float edgeDistance = .4f;
 
     void Start()
     {
